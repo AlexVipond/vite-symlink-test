3a8ef153f925958af3fcfc8c7c1167ce6642c94b
AlexVipond <alex.r.vipond@gmail.com>
2020-07-13 11:46:17 -0600

    include symlinks in repo

---
+++ vite-app/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>{{ foo }}</div>
+  <div>{{ bar }}</div>
+</template>
+
+<script>
+import { foo } from '@vite-symlink-test/foo'
+import { bar } from '@vite-symlink-test/bar'
+
+import { ref } from 'vue'
+
+export default {
+  name: 'App',
+  setup() {
+    return {
+      foo: ref(foo),
+      bar: ref(bar),
+    }
+  }
+}
+</script>
