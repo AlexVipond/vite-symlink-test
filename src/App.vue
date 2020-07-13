d2c9893626588cf97cc5660ea8e0ee54627d0b74
AlexVipond <alex.r.vipond@gmail.com>
2020-07-13 11:36:46 -0600

    setup reproduction

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+</template>
+
+<script>
+import { bar } from '@vite-symlink-test/bar'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  setup() {
+    console.log(bar)
+  }
+}
+</script>
